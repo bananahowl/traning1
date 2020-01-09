9b3eb2f24c0ab12622ee829fe509fcea051a57ff
A O <ahmedothman8@hotmail.com>
2020-01-09 20:16:51 +0100

    trabaa

--- src/main/java/rest/DemoResource.java
+++ src/main/java/rest/DemoResource.java
@@ -40,7 +40,7 @@ public class DemoResource {
     @Produces(MediaType.APPLICATION_JSON)
     @Path("all")
     public String allUsers() {
-        System.out.println("magic bois");
+        System.out.println("magic boiss");
         EntityManager em = EMF.createEntityManager();
         try {
             List<User> users = em.createQuery("select user from User user").getResultList();
